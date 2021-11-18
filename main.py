bda30c93c56d1e196deb4cedc8cdc8fa9402ea75
Cederic Martinez <cedzkii81@gmail.com>
2021-11-18 21:49:39 +0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from yeelight import discover_bulbs
 import json
 from yeelight import discover_bulbs
@@ -9,13 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    bulb = Bulb("192.168.1.3")
-    #bulb.toggle()
-    htmlcode = ""
-    for a in discover_bulbs():
-        print(a)
-        htmlcode = htmlcode + "<h3>" + a["ip"] + "</h3><br>"
-    return htmlcode
+    return render_template("index.html",bulblist= discover_bulbs())
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0')
