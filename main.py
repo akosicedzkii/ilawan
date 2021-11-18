947893fa8c50fbb151ac512159aa9bae768df30f
Cederic Martinez <cedzkii81@gmail.com>
2021-11-18 21:34:45 +0800

    test

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ app = Flask(__name__)
 def hello_world():
     bulb = Bulb("192.168.1.3")
     #bulb.toggle()
+    htmlcode = ""
     for a in discover_bulbs():
         print(a)
-    return "<p>Hello, World!</p>" 
+        htmlcode = htmlcode + "<h3>" + a["ip"] + "</h3><br>"
+    return htmlcode
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0')
