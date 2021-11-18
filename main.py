3e3553e303dcbfb69f5c85485d58c319020a794d
Cederic Martinez <cedzkii81@gmail.com>
2021-11-18 22:26:48 +0800

    test

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html",bulblist= discover_bulbs())
 
+@app.route("/toggle", methods=["POST"])
+def toogle_switch():
+    bulb = Bulb(request.form["ip_address"])
+    bulb.toggle()
+    return "True"
+
 if __name__ == '__main__':
     app.run( host='0.0.0.0')
