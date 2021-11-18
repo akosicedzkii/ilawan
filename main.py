93d128594fceeaa9c9b6ed5db20ac417f2188da7
Cederic Martinez <cedzkii81@gmail.com>
2021-11-18 21:23:27 +0800

    test

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     bulb = Bulb("192.168.1.3")
-    bulb.toggle()
-    return "<p>Hello, World!</p>" + json.dumps(discover_bulbs())
+    #bulb.toggle()
+    bulblist = discover_bulbs()
+    for bulblist in a:
+        print(a)
+    return "<p>Hello, World!</p>" 
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0')
