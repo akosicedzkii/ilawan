c7515f276fcf7e8e888b48554651ad12b6609232
Cederic Martinez <cedzkii81@gmail.com>
2021-12-24 21:41:56 +0800

    yeah

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from yeelight import discover_bulbs
 import json
 from yeelight import discover_bulbs,Bulb
-
 app = Flask(__name__)
 
 @app.route("/")
